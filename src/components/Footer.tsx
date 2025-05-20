d5074e4c114d880af426ec4926a2007acd8ac201
Antonio Thomas <antonio@antoniothomasjr.com>
2025-05-20 09:02:03 -0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,49 @@
+import { Box, Container, Stack, Link, Typography, IconButton } from '@mui/material';
+import { FaGithub, FaLinkedin, FaDownload } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <Box component="footer" sx={{ py: 4, bgcolor: `primary.main`, color: `primary.contrastText` }}>
+      <Container maxWidth="lg">
+        <Stack direction="row" justifyContent="center" spacing={3} sx={{ mb: 2 }}>
+          {[`Home`, `Projects`, `About`, `Contact`].map(section => (
+            <Link
+              key={section}
+              href={`#${section.toLowerCase()}`}
+              underline="hover"
+              color="inherit"
+            >
+              {section}
+            </Link>
+          ))}
+        </Stack>
+
+        <Stack direction="row" justifyContent="center" spacing={2} sx={{ mb: 2 }}>
+          <IconButton
+            component="a"
+            href="https://github.com/athomasjr"
+            target="_blank"
+            color="inherit"
+          >
+            <FaGithub />
+          </IconButton>
+          <IconButton
+            component="a"
+            href="https://linkedin.com/in/athomas-jr/"
+            target="_blank"
+            color="inherit"
+          >
+            <FaLinkedin />
+          </IconButton>
+          <IconButton component="a" href="/resume.pdf" download color="inherit">
+            <FaDownload />
+          </IconButton>
+        </Stack>
+
+        <Typography variant="body2" align="center">
+          © {new Date().getFullYear()} Antonio Thomas. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
       <Box component="main" sx={{ flexGrow: 1 }}>
         <Outlet />
       </Box>
+      <Footer />
     </Box>
   );
 }
