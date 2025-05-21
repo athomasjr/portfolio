1cfb14812c155cd89dcada705d8008f401263431
Antonio Thomas <antonio@antoniothomasjr.com>
2025-05-21 09:05:41 -0500

    add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { reactRouter } from '@react-router/dev/vite';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    reactRouter(),
+    netlifyPlugin(),
     svgr({
       svgrOptions: {
         ref: true,
